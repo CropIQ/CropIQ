ff720929038ddbc247140dc1e4c59633c26d5fe7
Rokas Braidokas <rokasbraidokas@gmail.com>
2025-05-24 00:32:11 +0300

    Added custom theme and some pictures.

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,26 @@
 "use client";
-import { createTheme } from '@mui/material/styles';
+import { createTheme, darken, lighten } from '@mui/material/styles';
 import { Roboto } from 'next/font/google';
+
+// import type { Palette, PaletteOptions } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    header: Palette['primary'] & {
+      borderColor: string;
+      navBarColor?: string;
+    },
+    notFound: Palette['primary']
+  }
+  interface PaletteOptions {
+    header?: PaletteOptions['primary'] & {
+      borderColor?: string;
+      navBarColor?: string;
+    },
+    notFound?: PaletteOptions['primary']
+  }
+}
+
 
 
 const roboto = Roboto({
@@ -10,9 +30,54 @@ const roboto = Roboto({
 });
 
 const theme = createTheme({
-  colorSchemes: { light: true, dark: true },  
+  colorSchemes: {
+    light: {
+      palette: {
+        header: {
+          main: '#F6F8FA',
+          light: lighten('#F6F8FA', 0.2),
+          dark: darken('#F6F8FA', 0.2),
+          contrastText: '#1F2328',
+          borderColor: darken('#F6F8FA', 0.2),
+          navBarColor: '#FD7C74',
+        },
+        notFound: {
+          main: '#F6F8FA',
+          light: lighten('#F6F8FA', 0.2),
+          dark: darken('#F6F8FA', 0.2),
+          contrastText: '#1F2328',
+        },
+        background: {
+          default: '#F6F8FA',
+          paper: '#F6F8FA',
+        },
+      },
+    },
+    dark: {
+      palette: {
+        header: {
+          main: '#010409',
+          light: lighten('#010409', 0.2),
+          dark: darken('#010409', 0.2),
+          contrastText: '#F0F6FC',
+          borderColor: lighten('#010409', 0.2),
+          navBarColor: '#FD7C74',
+        },
+        notFound: {
+          main: '#1B2430',
+          light: lighten('#1B2430', 0.2),
+          dark: darken('#1B2430', 0.2),
+          contrastText: '#F0F6FC',
+        },
+        background: {
+          default: '#1B2430',
+          paper: '#1B2430',
+        },
+      },
+    }
+  },  
   cssVariables: {
-    colorSchemeSelector: 'class',
+    colorSchemeSelector: 'class'
   },
   typography: {
     fontFamily: roboto.style.fontFamily,
